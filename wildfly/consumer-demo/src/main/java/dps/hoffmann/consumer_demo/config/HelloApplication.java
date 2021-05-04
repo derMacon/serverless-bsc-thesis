b1d1dd0e9d41e3015e58c164d2cad893ad7a49d9
derMacon <silash169@gmail.com>
2021-05-04 12:10:26 +0200

    added consumer project

---
+++ wildfly/consumer-demo/src/main/java/dps/hoffmann/consumer_demo/config/HelloApplication.java
@@ -0,0 +1,9 @@
+package dps.hoffmann.consumer_demo.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
