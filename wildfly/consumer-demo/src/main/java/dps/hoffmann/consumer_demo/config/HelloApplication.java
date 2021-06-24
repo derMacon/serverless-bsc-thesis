f3db55695042f369921ea8b9f4cb21f5d59234c4
hoffmann <silas.hoffmann@dps.de>
2021-06-24 22:27:37 +0200

    clean up

--- wildfly/consumer-demo/src/main/java/dps/hoffmann/consumer_demo/config/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package dps.hoffmann.consumer_demo.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
--- wildfly/producer-demo/src/main/java/dps/hoffmann/producer_demo/config/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package dps.hoffmann.producer_demo.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
